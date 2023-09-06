7722e33866aec79f92943549c187e64f19d1eee7
Andrius <andrius.rochalazarino@gmail.com>
2023-09-06 16:24:00 -0400

    style: getStarted

--- src/screens/GetStarted/index.tsx
+++ src/screens/GetStarted/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 import bille from "../../assets/bille.png";
 import Spotify from "../../assets/Spotify.svg";
@@ -12,8 +12,56 @@ export default function GetStarted() {
       <Image style={styles.imagem} source={bille} />
       <Spotify style={styles.logo} width={196} height={59} />
 
-      <View>
-        <Text>Enjoy listening to music</Text>
+      <View style={{ top: 350, alignItems: "center", paddingHorizontal: 53 }}>
+        <Text
+          style={
+            {
+              color: "#DADADA",
+              fontSize: 25,
+              lineHeight: 33,
+              fontWeight: 700,
+            } as any
+          }
+        >
+          Enjoy listening to music
+        </Text>
+        <Text
+          style={{
+            color: "#DADADA",
+            fontSize: 17,
+            lineHeight: 22,
+            fontWeight: "400",
+            textAlign: "center",
+            marginTop: 21,
+          }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          rhoncus feugiat turpis, vitae eleifend odio sollicitudin at.
+          Vestibulum consectetur sapien a ex dictum, sed cursus augue viverra.
+        </Text>
+
+        <TouchableOpacity
+          style={{
+            width: 329,
+            height: 92,
+            backgroundColor: "#42C83C",
+            borderRadius: 30,
+            marginTop: 37,
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <Text
+            style={{
+                color: "#F6F6F6",
+                fontSize: 22,
+                lineHeight: 29,
+                fontWeight: "700",
+                textAlign: "center",
+                textTransform: "capitalize"
+              }}
+          >Get Started</Text>
+        </TouchableOpacity>
       </View>
 
       <StatusBar style="auto" />
@@ -22,10 +70,10 @@ export default function GetStarted() {
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     backgroundColor: "#0D0C0C",
-    alignItems: "center"
+    alignItems: "center",
   },
   imagem: {
     width: "100%",
